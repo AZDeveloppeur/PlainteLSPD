bb4658a8c10fc147957015b9aacead940109715d
AZDeveloppeur <developpeur.az@gmail.com>
2022-12-26 10:46:41 +0100

    Fix some bug and update style

--- web/src/script.js
+++ web/src/script.js
@@ -1,6 +1,6 @@
 /*function save()
 {
-  var Texte = document.getElementById('ta');
+  var Texte = document.getElementById('text');
   alert(Texte.value);
   var a = document.getElementById("download");
   var file = new Blob([Texte.value], {type: 'text/plain'});
@@ -8,12 +8,12 @@
   a.download = 'plainte.txt';
 }*/
 
-let send = () => {
-  var Texte = document.getElementById('ta').value;
+function send() {
+  var Texte = document.getElementById('text').value;
   fetch(`http://az_framework/plainte_send`, {
     method: 'POST',
     body: JSON.stringify({
-      text : Text
+      text : Texte
     })
   })
 }
